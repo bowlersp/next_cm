79323ecf1189c95a3967296a36133618bd431073
bowler <m.bowler@f5.com>
2023-09-30 23:07:21 -0500

    added some error handling and better messaging

--- cm_as3_test.py
+++ cm_as3_test.py
@@ -35,17 +35,17 @@ function so that login / access token obtainment and REST
 method executions are handled consistently.
 '''
 def api_call(endpoint, method, uri, access_token, data=None):
-    headers = {"Content-Type": "application/json"}
+    if method in ["get", "patch", "put", "post", "delete"]:
+        headers = {"Content-Type": "application/json"}
 
-    # If no access token is provided, attempt to obtain
-    # one via the login process. Bail out if the login
-    # attempt fails. Otherwise, continue on and perform
-    # the respective REST method and return the
-    # JSON response object
-    if access_token != "":
-        headers["Authorization"] = f"Bearer {access_token}"
-    else:
-        if access_token == "":
+        # If no access token is provided, attempt to obtain
+        # one via the login process. Bail out if the login
+        # attempt fails. Otherwise, continue on and perform
+        # the respective REST method and return the
+        # JSON response object
+        if access_token != "":
+            headers["Authorization"] = f"Bearer {access_token}"
+        else:
             r = requests.post(f"https://{endpoint}/api/login", headers=headers, data=json.dumps({"username": username, "password": password}))
             if "access_token" in r.json().keys():
                 access_token = r.json()["access_token"]
@@ -54,19 +54,21 @@ def api_call(endpoint, method, uri, access_token, data=None):
                 status = r.json()["status"]
                 return f"Authoriation failed with a {status} error"
 
-    if method == "get":
-        response = requests.get(f"https://{endpoint}{uri}", headers=headers)
-    if method == "patch":
-        response = requests.patch(f"https://{endpoint}{uri}", headers=headers, data=json.dumps(data))
-        print(response.json())
-    if method == "put":
-        response = requests.put(f"https://{endpoint}{uri}", headers=headers, data=json.dumps(data))
-    if method == "post":
-        response = requests.post(f"https://{endpoint}{uri}", headers=headers, data=json.dumps(data))
-    if method == "delete":
-        response = requests.delete(f"https://{endpoint}{uri}", headers=headers)
+        if method == "get":
+            response = requests.get(f"https://{endpoint}{uri}", headers=headers)
+        elif method == "patch":
+            response = requests.patch(f"https://{endpoint}{uri}", headers=headers, data=json.dumps(data))
+            print(response.json())
+        elif method == "put":
+            response = requests.put(f"https://{endpoint}{uri}", headers=headers, data=json.dumps(data))
+        elif method == "post":
+            response = requests.post(f"https://{endpoint}{uri}", headers=headers, data=json.dumps(data))
+        elif method == "delete":
+            response = requests.delete(f"https://{endpoint}{uri}", headers=headers)
 
-    return response.json()
+        return response.status_code, response.json()
+    else:
+        return 400, f"Invalid method '{method}'"
 
 '''
 Search the application services declarations
@@ -74,31 +76,34 @@ for a specific tenant name and return its ID
 '''
 def get_declaration_by_name(name):
     uri = "/mgmt/shared/appsvcs/declare"
-    r = api_call(endpoint=endpoint, method="get", uri=uri, access_token="", data="")
+    status_code, r = api_call(endpoint=endpoint, method="get", uri=uri, access_token="", data="")
 
     if "_embedded" in r:
         appsvcs = r["_embedded"]["appsvcs"]
         for appsvc in appsvcs:
             if appsvc["tenant_name"] == name:
-                return appsvc["id"]
+                return True, appsvc["id"]
 
-    return f"Unable to find deploymant with tenant name {name}"
+    return False, f"Unable to find deploymant with tenant name {name}"
 
 '''
 POST an AS3 declaration to the CM API
 '''
 def post_declaration(declaration):
     uri = "/mgmt/shared/appsvcs/declare"
-    r = api_call(endpoint=endpoint, method="post", uri=uri, access_token="", data=declaration)
+    status_code, r = api_call(endpoint=endpoint, method="post", uri=uri, access_token="", data=declaration)
     
-    return r["id"]
+    if status_code == 400:
+        return False, r
+    else:
+        return True, r["id"]
 
 '''
 PATCH an AS3 declaration in the CM API
 '''
 def patch_declaration(declaration):
     uri = "/mgmt/shared/appsvcs/declare"
-    r = api_call(endpoint=endpoint, method="patch", uri=uri, access_token="", data=declaration)
+    status_code, r = api_call(endpoint=endpoint, method="patch", uri=uri, access_token="", data=declaration)
 
     return r["id"]
 
@@ -108,7 +113,7 @@ PUT an AS3 declaration to the CM
 '''
 def put_declaration(declaration_id, declaration):    
     uri = f"/mgmt/shared/appsvcs/declare/{declaration_id}"
-    r = api_call(endpoint=endpoint, method="put", uri=uri, access_token="", data=declaration)
+    status_code, r = api_call(endpoint=endpoint, method="put", uri=uri, access_token="", data=declaration)
 
     return r["id"]
 
@@ -117,7 +122,7 @@ DELETE an AS3 declaration from the CM API
 '''
 def delete_declaration(declaration_id):
     uri = f"/mgmt/shared/appsvcs/declare/{declaration_id}"
-    r = api_call(endpoint=endpoint, method="delete", uri=uri, access_token="")
+    status_code, r = api_call(endpoint=endpoint, method="delete", uri=uri, access_token="")
 
     return r["message"]
 
@@ -127,10 +132,21 @@ from the CM API
 '''
 def deploy_declaration(declaration_id, instances):
     uri = f"/mgmt/shared/appsvcs/declare/{declaration_id}/deployments"
+    instance_count = len(instances)
+    success_count = 0
+    failed_instances = []
     for instance in instances:
         data = {"target":f"{instance}"}
-        r = api_call(endpoint=endpoint, method="post", uri=uri, access_token="", data=data)
-    return r
+        status_code, r = api_call(endpoint=endpoint, method="post", uri=uri, access_token="", data=data)
+        if status_code == 202:
+            success_count += 1
+        else:
+            failed_instances.append(instance)
+    
+    if success_count == instance_count:
+        return True, r
+    else:
+        return False, f"{success_count} of {instance_count} were successful. The following failed: {', '.join(failed_instances)}"
 
 '''
 Run through the following sequence of events:
@@ -152,13 +168,20 @@ def main():
 
     # The creation and deployment of a declaration
     print("Sending AS3 declaration to CM API")
-    declaration_id = post_declaration(v1_declaration)
-    print(f"AS3 Declaration with ID {declaration_id} has been created\n")
+    as3_v1_created, declaration_id = post_declaration(v1_declaration)
+    if as3_v1_created:
+        print(f"AS3 Declaration with ID {declaration_id} has been created\n")
+    else:
+        print(f"AS3 Declaration creation failed with message: {declaration_id}")
+        return
 
-    instances = ["10.1.1.11"]
+    instances = ["10.1.1.11", "10.1.1.100"]
     print(f"Deploying v1 AS3 declaration ID {declaration_id} to {', '.join(instances)}")
-    deploy_result = deploy_declaration(declaration_id, instances)
-    print(f"Deployment result: {deploy_result}\n")
+    deploy_success, deploy_message = deploy_declaration(declaration_id, instances)
+    if deploy_success:
+        print(f"AS3 Deployment succeeded with result: {deploy_message}\n")
+    else:
+        print(f"AS3 Deployment failed with message: {deploy_message}")
 
     # Execute a brief pause while the declaration is consumed and deployed
     sleep(2)
@@ -166,8 +189,12 @@ def main():
     # Attempt retrieving a declaration by tenant name
     tenant_name = "testTenant001"
     print(f"Searching AS3 declarations for tenant named '{tenant_name}'")
-    declaration_id = get_declaration_by_name(tenant_name)
-    print(f"Successfully found AS3 tenant {tenant_name} with ID {declaration_id}\n")
+    search_success, declaration_id = get_declaration_by_name(tenant_name)
+    if search_success:
+        print(f"Successfully found AS3 tenant {tenant_name} with ID {declaration_id}\n")
+    else:
+        print(f"AS3 tenant search failed with message: {declaration_id}")
+        return
 
     # Pause the flow to allow validation within CM UI
     # or testing of the deployed declaration
